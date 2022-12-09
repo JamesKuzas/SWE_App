050bdfdf9bb6c23b4abc0cd4a7fa2174abba69eb
James Kuzas <JRKuzas@student.fullsail.edu>
2022-12-08 20:41:42 -0600

    Add project files.

---
+++ SWE_App/App.cpp
@@ -0,0 +1,11 @@
+#include "App.h"
+
+wxIMPLEMENT_APP(App);
+
+bool App::OnInit()
+{
+	window = new Window();
+	window->Show();
+
+	return true;
+}
---
+++ SWE_App/Window.cpp
@@ -0,0 +1,7 @@
+#include "Window.h"
+
+Window::Window() : 
+	wxFrame(nullptr, 0, "Calculator", wxPoint(400,200), wxSize(400,400))
+{
+
+}
