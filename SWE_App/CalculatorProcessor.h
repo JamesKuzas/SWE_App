811623a4492c604a7e9536eab893559b3642b970
James Kuzas <JRKuzas@student.fullsail.edu>
2022-12-15 21:50:44 -0600

    The start of code in CalculatorProcessor.

--- SWE_App/CalculatorProcessor.h
+++ SWE_App/CalculatorProcessor.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <vector>
+#include <string>
 class CalculatorProcessor
 {
+private:
+	static CalculatorProcessor* _processor;
+	std::vector<std::string> _math;
+	CalculatorProcessor();
+public:
+	CalculatorProcessor();
 };
 
